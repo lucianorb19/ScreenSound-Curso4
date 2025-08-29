f1ed097fe209ca0c8c7a63d0daaf882469603d33
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-08-29 10:42:42 -0300

    Formulário de login - sem cookie

---
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -0,0 +1,31 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+//CLASSE JÁ INICALIZA COM O IHttpClientFactory
+public class AuthAPI(IHttpClientFactory factory)
+{
+    private readonly HttpClient _httpClient = factory.CreateClient("API");
+
+    
+    public async Task<AuthResponse> LoginAsync(string email, string senha)
+    {
+        var response = await _httpClient.PostAsJsonAsync("auth/login", new
+        {
+            email,
+            password = senha
+        });
+
+        //SE A TENTATIVA DE LOGIN FOR BEM SUCEDIDA
+        if (response.IsSuccessStatusCode)
+        {   
+            return new AuthResponse{Sucesso = true};
+        }
+
+        //SE NÃO
+        return new AuthResponse{Sucesso = false, Erro="Erro no login" };
+    }
+
+   
+}
