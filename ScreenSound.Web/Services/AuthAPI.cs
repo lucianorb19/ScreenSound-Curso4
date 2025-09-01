fb3781be4669ae146c6f323ea06df05b6d5c4cc2
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-09-01 16:16:00 -0300

    Estado de autenticação para Login

--- ScreenSound.Web/Services/AuthAPI.cs
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -1,14 +1,59 @@
-﻿using ScreenSound.Web.Response;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
+using System.Security.Claims;
 
 namespace ScreenSound.Web.Services;
 
 //CLASSE JÁ INICALIZA COM O IHttpClientFactory
-public class AuthAPI(IHttpClientFactory factory)
+public class AuthAPI(IHttpClientFactory factory) : AuthenticationStateProvider
 {
+    //PROPRIEDADES - CAMPOS
     private readonly HttpClient _httpClient = factory.CreateClient("API");
 
-    
+    //DEMAIS MÉTODOS
+
+    //CONFIGURAÇÃO ESTADO DE AUTENTICAÇÃO
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        //ClaimsPrincipal VAZIO - PESSOA SEM AUTENTICAÇÃO / SEM ESTADO DE AUTENTICAÇÃO
+        var pessoa = new ClaimsPrincipal();
+
+        //Rota "auth/manage/info" DO IDENTITY RETORNA SE USUÁRIO ESTÁ LOGADO OU NÃO
+        var response = await _httpClient.GetAsync("auth/manage/info");
+
+        //SE HOUVER USUÁRIO LOGADO
+        if (response.IsSuccessStatusCode)
+        {
+            //SALVO INFORMAÇÕES DO USUÁRIO EM OBJETO InfoPessoaResponse info
+            var info = await response.Content.ReadFromJsonAsync<InfoPessoaResponse>();
+        
+            //CRIO VETOR DE CLAIMs COM DUS CLAIMs
+            Claim[] dados = 
+                [
+                    //A PRIMEIRA É PADRÃO DA MICROSOFT Name - EMAIL
+                    //EMAIL NESSE ESTADO É A PRINCIPAL INFORMAÇÃO DE AUTENTICAÇÃO
+                    new Claim(ClaimTypes.Name, info.Email),
+                    //SEGUNDA CLAIM É O PRÓPRIO EMAIL
+                    new Claim(ClaimTypes.Email, info.Email)
+                ];
+            //OBJETO ClaimIdentity QUE AGRUPA TODOS OS DADOS DA PESSOA
+            //E INDICA TAMBÉM NUMA STRING QUAL TIPO DE AUTENTICAÇÃO ESTÁ SENDO USADA
+            var identity = new ClaimsIdentity(dados, "Cookies");
+
+            //COM TUDAS AS INFORMAÇÕES PREENCHIDAS
+            //PREENCHO O OBJETO ClaimsPrincipal
+            //ClaimsPrincipal PREENCHIDO - PESSOA AUTENTICADA / ESTADO DE AUTENTICAÇÃO OK
+            pessoa = new ClaimsPrincipal(identity);
+        }
+
+        //RETORNO ESTADO DE AUTENTICAÇÃO 
+        //VAZIO OU AUTENTICADO - A DEPENDER SE HÁ USUÁRIO LOGADO NO SISTEMA
+        return new AuthenticationState(pessoa);
+    }
+
+
+    //LOGIN IDENTITY NA API
     public async Task<AuthResponse> LoginAsync(string email, string senha)
     {
         var response = await _httpClient.PostAsJsonAsync("auth/login?useCookies=true", new
@@ -19,7 +64,10 @@ public class AuthAPI(IHttpClientFactory factory)
 
         //SE A TENTATIVA DE LOGIN FOR BEM SUCEDIDA
         if (response.IsSuccessStatusCode)
-        {   
+        {
+            //MUDANDA ESTADO DE AUTENTICAÇÃO
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+
             return new AuthResponse{Sucesso = true};
         }
 
