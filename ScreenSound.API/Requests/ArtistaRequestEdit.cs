561fd2891d369fc49dcf5d45171d776be3827b4b
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-09-11 15:33:35 -0300

    Update ArtistaRequestEdit.cs

--- ScreenSound.API/Requests/ArtistaRequestEdit.cs
+++ ScreenSound.API/Requests/ArtistaRequestEdit.cs
@@ -1,4 +1,5 @@
 ﻿namespace ScreenSound.API.Requests;
 
-public record ArtistaRequestEdit(int Id,string nome,string bio,string? fotoPerfil)
+public record ArtistaRequestEdit(int Id, string nome,string bio,
+                                 string? fotoPerfil)
     : ArtistaRequest(nome,bio,fotoPerfil);
